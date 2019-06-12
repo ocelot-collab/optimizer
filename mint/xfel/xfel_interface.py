d14d227bc8c12971c345a2cf40aebce5786a2195
sergey-tomin <tomin.sergey@gmail.com>
2019-06-12 18:17:46 +0200

    removed database and extended number of parameters to save in json file

--- mint/xfel/xfel_interface.py
+++ mint/xfel/xfel_interface.py
@@ -18,6 +18,8 @@ import subprocess
 import base64
 from mint.opt_objects import MachineInterface, Device
 from collections import OrderedDict
+from datetime import datetime
+import json
 
 class AlarmDevice(Device):
     """
@@ -41,6 +43,7 @@ class XFELMachineInterface(MachineInterface):
 
         path2root = os.path.abspath(os.path.join(__file__ , "../../../.."))
         self.config_dir = os.path.join(path2root, "config_optim")
+
     def get_value(self, channel):
         """
         Getter function for XFEL.
@@ -65,6 +68,125 @@ class XFELMachineInterface(MachineInterface):
 
     def get_charge(self):
         return self.get_value("XFEL.DIAG/CHARGE.ML/TORA.25.I1/CHARGE.SA1")
+
+    def get_sases(self):
+        try:
+            sa1 = self.get_value("XFEL.FEL/XGM/XGM.2643.T9/INTENSITY.SA1.SLOW.TRAIN")
+        except:
+            sa1 = None
+        try:
+            sa2 = self.get_value("XFEL.FEL/XGM/XGM.2595.T6/INTENSITY.SLOW.TRAIN")
+        except:
+            sa2 = None
+        try:
+            sa3 = self.get_value("XFEL.FEL/XGM/XGM.3130.T10/INTENSITY.SA3.SLOW.TRAIN")
+        except:
+            sa3 = None
+        return [sa1, sa2, sa3]
+
+    def get_beam_energy(self):
+        try:
+            tld = self.get_value("XFEL.DIAG/BEAM_ENERGY_MEASUREMENT/TLD/ENERGY.DUD")
+        except:
+            tld = None
+        #t3 = self.get_value("XFEL.DIAG/BEAM_ENERGY_MEASUREMENT/T3/ENERGY.SA2")
+        #t4 = self.get_value("XFEL.DIAG/BEAM_ENERGY_MEASUREMENT/T4/ENERGY.SA1")
+        #t5 = self.get_value("XFEL.DIAG/BEAM_ENERGY_MEASUREMENT/T5/ENERGY.SA2")
+        try:
+            t4d = self.get_value("XFEL.DIAG/BEAM_ENERGY_MEASUREMENT/T4D/ENERGY.SA1")
+        except:
+            t4d = None
+        try:
+            t5d = self.get_value("XFEL.DIAG/BEAM_ENERGY_MEASUREMENT/T5D/ENERGY.SA2")
+        except:
+            t5d = None
+        return [tld, t4d, t5d]
+
+    def get_wavelength(self):
+        try:
+            sa1 = self.get_value("XFEL.FEL/XGM.PHOTONFLUX/XGM.2643.T9/WAVELENGTH")
+        except:
+            sa1 = None
+        try:
+            sa2 = self.get_value("XFEL.FEL/XGM.PHOTONFLUX/XGM.2595.T6/WAVELENGTH")
+        except:
+            sa2 = None
+        try:
+            sa3 = self.get_value("XFEL.FEL/XGM.PHOTONFLUX/XGM.3130.T10/WAVELENGTH")
+        except:
+            sa3 = None
+        return [sa1, sa2, sa3]
+
+    def get_ref_sase_signal(self):
+        try:
+            sa1 = self.get_value("XFEL.FEL/XGM/XGM.2643.T9/INTENSITY.SA1.SLOW.TRAIN")
+        except:
+            sa1 = None
+        try:
+            sa2 = self.get_value("XFEL.FEL/XGM/XGM.2595.T6/INTENSITY.SLOW.TRAIN")
+        except:
+            sa2 = None
+        #try:
+        #    sa3 = self.get_value("XFEL.FEL/XGM.PHOTONFLUX/XGM.3130.T10/WAVELENGTH")
+        #except:
+        #    sa3 = None
+        return [sa1, sa2]
+
+    def write_data(self, method_name, objective_func, devices=[], maximization=False, max_iter=0):
+        """
+        Save optimization parameters to the Database
+
+        :param method_name: (str) The used method name.
+        :param objective_func: (Target) The Target class object.
+        :param devices: (list) The list of devices on this run.
+        :param maximization: (bool) Whether or not the data collection was a maximization. Default is False.
+        :param max_iter: (int) Maximum number of Iterations. Default is 0.
+
+        :return: status (bool), error_msg (str)
+        """
+
+        if objective_func is None:
+            return False, "Objective Function required to save data."
+
+
+        dump2json = {}
+
+        for dev in devices:
+            dump2json[dev.eid] = dev.values
+
+        dump2json["method"] = method_name
+        dump2json["dev_times"] = devices[0].times
+        dump2json["obj_values"] = objective_func.values
+        dump2json["obj_times"] = objective_func.times
+        dump2json["maximization"] = maximization
+        dump2json["std"] = objective_func.std_dev
+        dump2json["nreadings"] = objective_func.nreadings
+        dump2json["function"] = objective_func.eid
+        dump2json["beam_energy"] = self.get_beam_energy()
+        dump2json["wavelength"] = self.get_wavelength()
+        try:
+            dump2json["ref_sase"] = [objective_func.ref_sase[0], objective_func.ref_sase[-1]]
+        except Exception as e:
+            print("ERROR. Read ref sase: " + str(e))
+
+
+        try:
+            dump2json["charge"] = self.get_charge()
+        except Exception as e:
+            print("ERROR. Read charge: " + str(e))
+            dump2json["charge"] = None
+
+        if not os.path.exists(self.path2jsondir):
+            os.makedirs(self.path2jsondir)
+
+        filename = os.path.join(self.path2jsondir, datetime.now().strftime("%Y-%m-%d %H-%M-%S") + ".json")
+        try:
+            with open(filename, 'w+') as f:
+                json.dump(dump2json, f)
+        except Exception as e:
+            print("ERROR. Could not write data: " + str(e))
+        return True, ""
+
 
     def send_to_logbook(self, *args, **kwargs):
         """
@@ -243,8 +365,6 @@ class TestMachineInterface(XFELMachineInterface):
         Y = np.zeros(len(bpms))
         return X, Y
 
-    def get_charge(self):
-        return 0
 
     @staticmethod
     def send_to_logbook(*args, **kwargs):
