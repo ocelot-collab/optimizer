40e7c72eca7a9cfdb427e1a8b4b75a53810e0b9b
sergey-tomin <tomin.sergey@gmail.com>
2019-06-18 10:49:42 +0200

    minor

--- op_methods/__init__.py
+++ op_methods/__init__.py
@@ -4,7 +4,8 @@ __all__ = ['Simplex', 'SimplexNorm',
            'Powell', 'CustomMinimizer',
            'GaussProcessSKLearn', 'GaussProcess',
            'ESMin',
-           "GPgpy"]
+           #"GPgpy",
+           ]
 
 from op_methods.simplex import *
 from op_methods.powell import *
@@ -12,4 +13,4 @@ from op_methods.custom_minimizer import *
 from op_methods.gp_sklearn import *
 from op_methods.gp_slac import *
 from op_methods.es import *
-from op_methods.gp_gpy import *
+#from op_methods.gp_gpy import *
