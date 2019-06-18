5f372663d8d0bf6d840b76efd20e87bc8b2c6dda
sergey-tomin <tomin.sergey@gmail.com>
2019-06-18 10:33:01 +0200

    method to define the self.dev_steps was moved to simplex.preprocess

--- op_methods/simplex.py
+++ op_methods/simplex.py
@@ -7,7 +7,25 @@ class Simplex(Minimizer):
         super(Simplex, self).__init__()
         self.xtol = 1e-5
         self.dev_steps = None
+    
+    def preprocess(self):
+        """
+        defining attribute self.dev_steps
 
+        :return:
+        """
+
+        self.dev_steps = []
+        for dev in self.devices:
+            if "istep" not in dev.__dict__:
+                self.dev_steps = None
+                return
+            elif dev.istep is None or dev.istep == 0:
+                self.dev_steps = None
+                return
+            else:
+                self.dev_steps.append(dev.istep)
+    
     def minimize(self,  error_func, x):
         #print("start seed", np.count_nonzero(self.dev_steps))
         if self.dev_steps == None or len(self.dev_steps) != len(x):
